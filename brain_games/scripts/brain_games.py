7550d1b0980f218d343f9984c7596da3471f3926
AlexanderUridin <a89154850307@gmail.com>
2024-01-01 23:44:07 +0300

    add prompt lib, cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+
 def main():
     print(f'Welcome to the Brain Games!')
-    
+    welcome_user()
+
 if __name__ == '__main__':
     main()
